9776b927d0b414d54cddc953511c53d0f33f053a
KhoaNguyen55 <git@khoa-nguyen.net>
2025-10-19 19:27:31 -0400

    use trash in oil.nvim

--- stow/dot-config/nvim/lua/custom/plugins/oil.lua
+++ stow/dot-config/nvim/lua/custom/plugins/oil.lua
@@ -2,6 +2,7 @@ return {
   'stevearc/oil.nvim',
   config = function()
     require('oil').setup {
+      delete_to_trash = true,
       view_options = { show_hidden = true },
       keymaps = {
         ['g?'] = 'actions.show_help',
